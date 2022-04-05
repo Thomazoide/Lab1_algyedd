fbff44148f098b7ddb3b7fe63fbd13b8fc59b413
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2022-04-04 21:13:58 -0400

    Guardado de datos y eliminacion de duplicados sumando la cantidad de entraas de estos exitosa.

--- lab.c
+++ lab.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include<string.h>
-#define max 200 /*Variable que almacena un valor maximo para ser usado a la hora de leer una linea de un archivo*/
+#define max 200 /*Macro que almacena un valor maximo para ser usado a la hora de leer una linea de un archivo*/
 
 typedef struct nodo{
-    char linea[max];
+    char rut[max];
+    char nombre[max];
+    int cantEntradas;
     struct nodo *sig;
 }lista;
 
@@ -12,7 +14,13 @@ lista *raiz = NULL;
 
 void insertar(char linea[]){
     lista *nuevo = malloc(sizeof(lista));
-    strcpy(nuevo->linea, linea);
+    char *token;
+    token = strtok(linea, ",");
+    strcpy(nuevo->rut, token);
+    token = strtok(NULL, ",");
+    strcpy(nuevo->nombre, token);
+    token = strtok(NULL, ",");
+    nuevo->cantEntradas = atoi(token);
     if(raiz == NULL){
         raiz = nuevo;
         nuevo->sig = NULL;
@@ -23,12 +31,40 @@ void insertar(char linea[]){
     }
 }
 
+void quitarDuplicados(){
+    lista *recorrido, *duplicado;
+    recorrido = raiz;
+    while(recorrido != NULL){
+        if(strcmp(recorrido->rut, recorrido->sig->rut) == 0){
+            duplicado = recorrido->sig;
+            recorrido->cantEntradas += recorrido->sig->cantEntradas;
+            recorrido->sig = recorrido->sig->sig;
+            free(duplicado);
+        }
+        else{
+            recorrido = recorrido->sig;
+        }
+    }
+}
+
 void imprimir(){
     lista *recorrido = raiz;
     printf("\tCompras de usuarios\n");
+    int i = 0;
     while(recorrido != NULL){
-        printf("1.- %s\n", recorrido->linea);
+        printf("\n%i .- \nRut: %s\nNombre: %s\nCantidad de entradas: %i",i,recorrido->rut,recorrido->nombre,recorrido->cantEntradas);
         recorrido = recorrido->sig;
+        i++;
+    }
+}
+
+void limpiar(){
+    lista *recorrido, *basura;
+    recorrido = raiz;
+    while(recorrido != NULL){
+        basura = recorrido;
+        recorrido = recorrido->sig;
+        free(basura);
     }
 }
 
@@ -40,4 +76,8 @@ int main(){
         fgets(linea, max, f);
         insertar(linea);
     }
+    quitarDuplicados();
+    imprimir();
+    fclose(f);
+    limpiar();
 }
